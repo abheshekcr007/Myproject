d4b4a16056b27c1a89ab27e0f99841a09378b8a6
abhe <cabheshek@gmail.com>
2016-06-16 15:02:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/abhe/sffghth/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.abhe.sffghth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+
+        // Instance of ImageAdapter Class
+        gridView.setAdapter(new ImageAdapter(this));
+    }
+}
